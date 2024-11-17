42af30e5a7a6b5f08a0613914c3f5a13ac35adef
Sanjay Karki <karkisanjay2002@gmail.com>
2024-11-17 12:44:37 +0545

    added test case for authentication

---
+++ tests/index.test.js
@@ -0,0 +1,111 @@
+const axios = require("axios");
+
+const BACKEND_URL = "http://localhost:3000";
+const WS_URL = "ws://localhost:3001";
+
+// Describe blocks
+
+const axios = {
+    post: async (...args) => {
+        try {
+            const res = await axios2.post(...args)
+            return res
+        } catch(e) {
+            return e.response
+        }
+    },
+    get: async (...args) => {
+        try {
+            const res = await axios2.get(...args)
+            return res
+        } catch(e) {
+            return e.response
+        }
+    },
+    put: async (...args) => {
+        try {
+            const res = await axios2.put(...args)
+            return res
+        } catch(e) {
+            return e.response
+        }
+    },
+    delete: async (...args) => {
+        try {
+            const res = await axios2.delete(...args)
+            return res
+        } catch(e) {
+            return e.response
+        }
+    },
+}
+
+
+describe("Authentication", () => {
+  test("User is able to sign up only once", async () => {
+    const username = "sanjay" + Math.random();
+    const password = "123456";
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+
+    expect(response.statusCode).toBe(200);
+
+    const updatedresponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+
+    expect(updatedresponse.statusCode).toBe(400);
+  });
+  test('Signup request fails if the username is empty', async () => {
+    const username = `sanjay-${Math.random()}` 
+    const password = "123456"
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+        password
+    })
+
+    expect(response.status).toBe(400)
+  })
+  test('Signin succeeds if the username and password are correct', async() => {
+    const username = `sanjay-${Math.random()}`
+    const password = "123456"
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+        username,
+        password,
+        type: "admin"
+    });
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+        username,
+        password
+    });
+
+    expect(response.status).toBe(200)
+    expect(response.data.token).toBeDefined()
+    })
+
+   test('Signin fails if the username and password are incorrect', async() => {
+        const username = `sanjay-${Math.random()}`
+        const password = "123456"
+
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+            role: "admin"
+        });
+
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+            username: "WrongUsername",
+            password
+        })
+
+        expect(response.status).toBe(403)
+    })
+
+});
